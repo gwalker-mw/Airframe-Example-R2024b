60b8aad89258272b69a6164b56f43dab91756da6
gwalker <gwalker@MW-PF618MB3>
2026-09-08 09:52:40 +0100

    Fixed gain, added logging

--- tests/f14_airframe_test.m
+++ tests/f14_airframe_test.m
@@ -1,7 +1,7 @@
 classdef f14_airframe_test < matlab.unittest.TestCase
     %f14_airframe_test   MATLAB unit test for the f14_airframe model
     
-    % Copyright 2013-2018 The MathWorks, Inc.
+    % Copyright 2013-2026 The MathWorks, Inc.
     
     properties
         AirframeBusData
